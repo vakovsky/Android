631416af8d51921ea8a9fa18c004ae860a136fbb
Dimiter Vakovsky <vakovsky@gmail.com>
2022-02-25 15:35:19 +0200

    Create MainActivitySec.java

---
+++ MainActivitySec.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivitySec extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sec);
+        Intent intent = getIntent();
+        int val1 = intent. getIntExtra("key1", 0);
+        int val2 = intent. getIntExtra("key2", 0);
+        int c = val1 * val2;
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("key3", c);
+        setIntent(resultIntent);
+        finish();
+    }
+}
