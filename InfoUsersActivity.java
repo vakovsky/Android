2d0f7fdd08b80e0063d1df9dfbc9c8b180a5f013
Dimiter Vakovsky <vakovsky@gmail.com>
2022-03-10 11:27:49 +0200

    Update InfoUsersActivity.java

--- InfoUsersActivity.java
+++ InfoUsersActivity.java
@@ -1,17 +1,21 @@
 package bg.tcom.c12v1gr.demo122applivation;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class InfoUsersActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfoUsersActivity extends ListActivity {
     EditText etUsername;
-    EditText etPassword;
+    EditText etEmail;
     TextView tvUsersInfo;
     Button bt1;
     @Override
@@ -20,7 +24,7 @@ public class InfoUsersActivity extends Activity {
         setContentView(R.layout.activity_info_users);
 
         etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
         tvUsersInfo = findViewById(R.id.usersinfo);
 
         bt1 = findViewById(R.id.button1);
@@ -33,19 +37,34 @@ public class InfoUsersActivity extends Activity {
         if(c.moveToFirst()) {
             do {
                 b.append(c.getString(c.getColumnIndex("user")));
-                b.append(c.getString(c.getColumnIndex("pass")));
+                b.append(c.getString(c.getColumnIndex("email")));
                 b.append(" , ");
             } while (c.moveToNext());
         }
         String str = b.toString();
         tvUsersInfo.setText(str);
 
+        List<String> usersArray = new ArrayList<>();
+
+        if(c.moveToFirst()) {
+            do {
+                b.append(c.getString(c.getColumnIndex("user")));
+                b.append(c.getString(c.getColumnIndex("email")));
+                b.append(" , ");
+                usersArray.add(c.getString(c.getColumnIndex("user")) + " "
+                        +c.getString(c.getColumnIndex("email")));
+            } while (c.moveToNext());
+        }
+
+
+        this.setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, usersArray));
+
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-                infoUsers.insert(username, password);
+                String email = etEmail.getText().toString();
+                infoUsers.insert(username, email);
 
                 Toast.makeText(InfoUsersActivity.this, "Insert OK", Toast.LENGTH_LONG).show();
             }
