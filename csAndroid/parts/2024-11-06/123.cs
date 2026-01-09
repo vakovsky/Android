bd781a884ade8f6cc5cb15ce091c1993eb45ed05
Dimiter Vakovsky <vakovsky@gmail.com>
2026-01-09 18:27:22 +0200

    Update 123.cs

--- csAndroid/parts/2024-11-06/123.cs
+++ csAndroid/parts/2024-11-06/123.cs
@@ -4,19 +4,13 @@
   //2.2 Private files
 
 //Internal Storage
-string path1 = Path.Combine(
-    Application.Context.FilesDir.AbsolutePath,
-    "internal.txt"
-);
+string path1 = Path.Combine(Application.Context.FilesDir.AbsolutePath, "internal.txt");
 File.WriteAllText(path1, "Internal storage");
 
 //External – Private
 //Java.IO.File dir2 = Application.Context.GetExternalFilesDir(null);
 string dir2 = Application.Context.GetExternalFilesDir(null)?.AbsolutePath;
-string path2 = Path.Combine(
-    dir2,
-    "private.txt"
-    );
+string path2 = Path.Combine(dir2, "private.txt");
 File.WriteAllText(path2, "External private storage");
 
 //External – Public
@@ -25,3 +19,4 @@ Java.IO.File dir3 = Android.OS.Environment.GetExternalStoragePublicDirectory(
 );
 string path3 = Path.Combine(dir3.AbsolutePath, "public.txt");
 File.WriteAllText(path3, "External public storage");
+
